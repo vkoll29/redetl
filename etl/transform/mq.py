052a1d073777dabe56d2d2f3f0d2cf1bf23eb886
George Owino <fuhrer.go@gmail.com>
2024-06-25 12:01:29 +0300

    removed pyc files from tracking

--- etl/transform/mq.py
+++ etl/transform/mq.py
@@ -3,26 +3,23 @@ from src.utils.load_to_db import load_data
 from src.utils.convert_dtypes import get_sql_dtype
 
 
-def scenes_insert_staging(conn, df, container_name):
+def transform_mq(conn, df, container_name):
+    print(df.dtypes)
     # step 1: Drop unnecessary columns
     columns_to_drop = [
-        'StitchedImageURL',
-        'RawImagePath',
-        'RawImageNames',
-        'ImageQuality',
+        'ID',
+        'QuestionType',
+        'Text',
+        'Tags',
+        'ProductDetail',
         'ReExportStatus',
         'ReExportTime',
-        'ParentSceneUid',
-        'ImageCount',
-        'AssetSerialNumber',
-        'AssetEquipmentNumber',
-        'SceneCollectionMode',
-        'IsFacingAvailable',
-        'IsSelfValidated',
-        'IsClientValidated',
-        'IrImageUsable'
+        'ExpiryDate'
     ]
     df = df.drop(columns_to_drop, axis=1)
+
+    # step 2: Truncate options column to 200 characters
+    df['Options'] = df['Options'].str[:200]
 
     # step 2: Add Bottler column
     df = add_bottler_column(df, container_name)
@@ -31,24 +28,37 @@ def scenes_insert_staging(conn, df, container_name):
     new_order = [
         'SessionUID',
         'SceneUID',
-        'SceneDateTime',
-        'VerifiedOn',
-        'Source',
         'SceneType',
         'SubSceneType',
+        'ProgramID',
+        'ProgramName',
+        'ProgramItemID',
+        'ProgramItemName',
+        'QuestionID',
+        'QuestionCategory',
+        'QuestionText',
+        'LocalQuestionText',
+        'ParentQuestionID',
+        'Options',
+        'AnswerValue',
         'CreatedOnTime',
         'LastModifiedTime',
         'FileCreatedTime',
         'SliceStartTime',
         'SliceEndTime',
-        'Bottler',
+        'SceneLevel',
+        'SessionLevel',
+        'Description',
         'ReProcessedTime',
         'ReProcessedStatus',
+        'Bottler',
+        'EvidenceImageName',
+        'EvidenceImageURL'
     ]
-    df_tf = df.loc[:, new_order]
+    df = df.loc[:, new_order]
 
     # step 4: Convert dtypes to SQL types
-    df_tf = get_sql_dtype(df_tf)
+    df_tf = get_sql_dtype(df)
 
     # step 4: Insert data to staging table
-    load_data(df_tf, conn, 'stg.stageIRScenes')
+    load_data(df_tf, conn, 'stg.StageIRManualQuestions')
