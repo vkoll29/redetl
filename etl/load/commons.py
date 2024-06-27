549b21e9dc1c964b82db1685666119444a176243
George Owino <fuhrer.go@gmail.com>
2024-06-27 12:58:49 +0300

    Refactored all common ingestion tasks

---
+++ etl/load/commons.py
@@ -0,0 +1,24 @@
+from src.utils.convert_dtypes import get_sql_dtype
+from etl.load.load_to_db import load_data
+from etl.load.prep_landing import prep_landing_table
+from etl.load.clear_staging import clear_staging_table
+
+
+def execute_common_ops(conn, df, table):
+
+
+    # step 2: Convert dtypes to SQL types
+    df = get_sql_dtype(df)
+
+    # step 3: insert data to staging table
+    load_data(df, conn, f'stg.stage{table}')
+
+    # step 4: Prepare landing table
+    prep_landing_table(conn, table)
+
+    # step 5: Insert data to landing table
+    load_data(df, conn, f'stg.{table}')
+
+    # step 6: Truncate staging table
+    clear_staging_table(conn, table)
+
